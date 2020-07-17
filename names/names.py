4968d3348da0cf66aa2b895a14913b462ce3893b
Devanee <devaneereid@gmail.com>
2020-07-17 11:37:07 -0600

    final commit - tests now passing for reverse list

--- names/names.py
+++ names/names.py
@@ -21,7 +21,7 @@ for name_1 in names_1:
 
 for name_2 in names_2:
     if bts_node.contains(name_2):
-        duplicates.append(names_2[name_2] and names_1[name_1])
+        duplicates.append(names_2[name_2])
 
     # if name_1 == name_2:
     #     duplicates.append(name_1)
