4f37eec6653c0c4c5eda4afe0ee80cf03fa856e9
Devanee <devaneereid@gmail.com>
2020-07-17 10:42:51 -0600

    fixed the time on names.py, and added data_structure

--- names/names.py
+++ names/names.py
@@ -1,5 +1,5 @@
 import time
-
+from bts_data_structure import BSTNode
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,10 +13,18 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+bts_node = BSTNode(names_1[0])
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bts_node.insert(name_1)
+
+for name_2 in names_2:
+    if bts_node.contains(name_2):
+        duplicates.append(names_2[name_2] and names_1[name_1])
+
+    # if name_1 == name_2:
+    #     duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
